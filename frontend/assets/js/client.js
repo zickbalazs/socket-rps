a742065babc4790f1be344747f015b9e9203fbde
zickbalazs <balazs.zick2@gmail.com>
2022-11-24 18:20:32 +0100

    Rooms work

--- frontend/assets/js/client.js
+++ frontend/assets/js/client.js
@@ -1,19 +1,28 @@
 let socket = io();
-
-document.onload = ()=>{
-    socket.emit('roomRequest', )
-}
-
-
-socket.on('success', ()=>{
-    alert('Sikeres csatlakozás');
+let form = document.querySelector('form');
+let player;
+socket.on('cant',()=>{
+    document.querySelector('.alert').classList.remove('d-none');
 });
-socket.on('unauthorized', ()=>{
-    window.location.href='localhost:8080/';
+socket.on('can', async ()=>{
+    let htmldata = await (fetch('http://localhost:8080/game').then(r=>r.text()).then(data=>{return data}));
+    document.querySelector('main').innerHTML = htmldata;
+    SetUp();
+});
+socket.on('Users', (data)=>{
+    document.querySelector('#player').innerHTML = data.player;
+    document.querySelector('#opponent').innerHTML = data.opponent==undefined?"Várakozás ellenfélre...":data.opponent;
 })
-function GetReq(){
-    return {
-        player: document.querySelector('form').player.value,
-        room: document.querySelector('form').room.value
-    }
+
+document.querySelector('#join').addEventListener('click', ()=>{
+    console.log('trying to emit')
+    socket.emit('roomRequest', {
+        user: form.player.value,
+        room: form.room.value
+    });
+    player = form.player.value;
+})
+
+function SetUp(){
+    socket.emit('GetUsers');
 }
--- server.js
+++ server.js
@@ -12,46 +12,43 @@ app.use('/js', express.static(path.join(__dirname, './frontend/assets/js')))
 app.use('/bootstrap', express.static(path.join(__dirname, './node_modules/bootstrap/dist/')));
 app.use('/socketio', express.static(path.join(__dirname, './node_modules/socket.io/client-dist')));
 
-
+app.get('/game', (req,res)=>{
+    res.status(200).sendFile(path.join(__dirname, './frontend/assets/html/game.html'));
+})
 app.get('/', (req,res)=>{
-    ejs.renderFile('./frontend/views/pages/page.ejs', {needsSocket:false, p: 'index'},(err,data)=>{
+    ejs.renderFile('./frontend/views/pages/page.ejs', {needsSocket:true, p: 'index'},(err,data)=>{
         if (err) res.status(500).send(err.message);
         else res.status(200).send(data);
     })
 })
-
-app.post('/game', (req,res)=>{
-    if (Object.values(req.body).includes('')){
-        res.redirect('/')
-    }
-    else {
-        req.session.room = req.body.room;
-        req.session.user = req.body.player;
-        ejs.renderFile('./frontend/views/pages/page.ejs', {needsSocket:true, p:'game', game: req.session}, (err,data)=>{
-            if (err) res.status(500).send(err.message);
-            else{
-                
-
-                res.status(200).send(data);
-            } 
-        })
-    }
-})
-
 //io.sockets.adapter.rooms.get(data.room) <- szobák lekérdezése
-
-
 io.on('connection', (socket)=>{
     socket.on('roomRequest', (data)=>{
-        
+        if (io.sockets.adapter.rooms.get(data.room)==undefined || Array.from(io.sockets.adapter.rooms.get(data.room)).length<2){
+            socket.join(data.room);
+            socket.data = {
+                usr: data.user,
+                room: data.room
+            };
+            console.log('room:',socket.id,'=>', Array.from(io.sockets.adapter.rooms.get(data.room)));
+            socket.emit('can');
+        }
+        else socket.emit('cant')
     });
+    socket.on('GetUsers', ()=>{
+        socket.emit('Users', GetRoomUsers(socket.data.room));
+        socket.to(socket.data.room).emit('Users', GetRoomUsers(socket.data.room));
+    })
     socket.on('disconnect', (socket)=>{});
-})
-
-
+});
+function GetRoomUsers(roomID){
+    let names = [];
+    Array.from(io.sockets.adapter.rooms.get(roomID)).forEach(e=>{
+        names.push(io.sockets.sockets.get(e).data.usr);
+    })
+    return {
+        player: names[0],
+        opponent: names[1]
+    };
+}
 server.listen(8080);
-
-
-
-
-
