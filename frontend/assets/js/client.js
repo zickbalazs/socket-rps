f2cb18110856017f45d35694c610fe42ce0c71d5
zickbalazs <zickbalazs@turr.hu>
2022-11-25 10:34:27 +0100

    Játék working

--- frontend/assets/js/client.js
+++ frontend/assets/js/client.js
@@ -1,6 +1,7 @@
 let socket = io();
 let form = document.querySelector('form');
 let player;
+let room;
 socket.on('cant',()=>{
     document.querySelector('.alert').classList.remove('d-none');
 });
@@ -8,10 +9,17 @@ socket.on('can', async ()=>{
     let htmldata = await (fetch('http://localhost:8080/game').then(r=>r.text()).then(data=>{return data}));
     document.querySelector('main').innerHTML = htmldata;
     SetUp();
+    SetFunction();
 });
 socket.on('Users', (data)=>{
     document.querySelector('#player').innerHTML = data.player;
     document.querySelector('#opponent').innerHTML = data.opponent==undefined?"Várakozás ellenfélre...":data.opponent;
+})
+socket.on('announce', (data)=>{
+    console.log(data);
+    document.querySelector('#player1').innerHTML = data.score[0];
+    document.querySelector('#player2').innerHTML = data.score[1];
+    document.querySelector('#OpponentPick').innerHTML = data
 })
 
 document.querySelector('#join').addEventListener('click', ()=>{
@@ -21,8 +29,21 @@ document.querySelector('#join').addEventListener('click', ()=>{
         room: form.room.value
     });
     player = form.player.value;
+    room = form.room.value;
 })
 
 function SetUp(){
     socket.emit('GetUsers');
+}
+function SendToServer(which){
+    socket.emit('Choose', which);
+}
+function SetFunction(){
+    document.querySelector('#rock').addEventListener('click', ()=>{SendToServer('Rock'); document.querySelector('#rock').classList.add('current')});
+    document.querySelector('#paper').addEventListener('click', ()=>{SendToServer('Paper'); document.querySelector('#paper').classList.add('current')});
+    document.querySelector('#scissors').addEventListener('click', ()=>{SendToServer('Scissors'); document.querySelector('#scissors').classList.add('current')});
+    document.querySelector('#left').addEventListener('click', ()=>{
+        socket.emit('dc', room);
+        window.location.reload();        
+    })
 }
--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.get('/', (req,res)=>{
         else res.status(200).send(data);
     })
 })
+let RoomsData = new Map();
 //io.sockets.adapter.rooms.get(data.room) <- szobák lekérdezése
 io.on('connection', (socket)=>{
     socket.on('roomRequest', (data)=>{
@@ -30,7 +31,12 @@ io.on('connection', (socket)=>{
                 usr: data.user,
                 room: data.room
             };
-            console.log('room:',socket.id,'=>', Array.from(io.sockets.adapter.rooms.get(data.room)));
+            RoomsData.set(socket.data.room, {
+                choosen: [],
+                score: [0,0]
+            })
+            console.log(RoomsData.get(socket.data.room))
+            console.log('room:',socket.data.room,'=>', Array.from(io.sockets.adapter.rooms.get(data.room)));
             socket.emit('can');
         }
         else socket.emit('cant')
@@ -39,7 +45,38 @@ io.on('connection', (socket)=>{
         socket.emit('Users', GetRoomUsers(socket.data.room));
         socket.to(socket.data.room).emit('Users', GetRoomUsers(socket.data.room));
     })
-    socket.on('disconnect', (socket)=>{});
+    socket.on('Choose', (which)=>{
+        let chosen = RoomsData.get(socket.data.room)==undefined?[]:RoomsData.get(socket.data.room).choosen;
+        let score = RoomsData.get(socket.data.room)==undefined?[0, 0]:RoomsData.get(socket.data.room).score;
+        chosen[Array.from(io.sockets.adapter.rooms.get(socket.data.room)).findIndex(e=>e==socket.id)] = which;
+        if (RoomsData.get(socket.data.room).choosen.filter(e=>e).length==2){
+            RoomsData.set(socket.data.room, {
+                choosen:chosen,
+                score: EvalScore(chosen, score)
+            })
+            socket.to(socket.data.room).emit('announce', RoomsData.get(socket.data.room));
+            socket.emit('announce', RoomsData.get(socket.data.room));
+            RoomsData.set(socket.data.room, {
+                choosen: [],
+                score: score
+            });
+        }
+        else{
+            RoomsData.set(socket.data.room, {
+                choosen:chosen,
+                score: score
+            })
+        }
+    })
+    socket.on('dc', (room)=>{
+        console.log('dc')
+        socket.emit('Users', GetRoomUsers(room));
+        socket.to(socket.data.room).emit('Users', GetRoomUsers(room));
+        RoomsData.delete(socket.data.room)
+    })
+    socket.on('disconnect', ()=>{
+        RoomsData.delete(socket.data.room)
+    });
 });
 function GetRoomUsers(roomID){
     let names = [];
@@ -50,5 +87,20 @@ function GetRoomUsers(roomID){
         player: names[0],
         opponent: names[1]
     };
+}
+function EvalScore(array, score){
+    switch (true){
+        case array[0]==array[1]: return [score[0], score[1]];
+        //p1 conditions
+        case array[0]=='Rock'&&array[1]=='Paper': return [score[0], score[1]+1];
+        case array[0]=='Paper'&&array[1]=='Rock': return [score[0], score[1]+1];
+        case array[0]=='Scissors'&&array[1]=='Rock': return [score[0], score[1]+1];
+        //p2 conditions
+        case array[1]=='Rock'&&array[0]=='Paper': return [score[0]+1, score[1]];
+        case array[1]=='Paper'&&array[0]=='Rock': return [score[0]+1, score[1]];
+        case array[1]=='Scissors'&&array[0]=='Rock': return [score[0]+1, score[1]];
+        //errors
+        default: return score;
+    }
 }
 server.listen(8080);
